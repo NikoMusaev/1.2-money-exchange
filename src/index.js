3adf0496e133a797af3e2e4236e79cd0518ae39f
Niko Musaev <nikitoz9999@gmail..com>
2019-07-02 01:45:18 +0500

    Solving a task with a money exchange (https://github.com/SIARHEI-SMANTSAR/new-money-exchange)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if ( currency > 10000 ) return  {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if ( currency < 1 ) return {};
+    var coins = {};
+    var left = currency;
+
+    if (left >= 50) {
+        coins.H = Math.trunc(left / 50);
+        left -= coins.H * 50;
+    }
+    if (left >= 25) {
+        coins.Q = Math.trunc(left / 25);
+        left -= coins.Q * 25;
+    }
+    if (left >= 10) {
+        coins.D = Math.trunc(left / 10);
+        left -= coins.D * 10;
+    }
+    if (left >= 5) {
+        coins.N = Math.trunc(left / 5);
+        left -= coins.N * 5;
+    }
+    if (left >= 1) {
+        coins.P = Math.trunc(left / 1);
+        left -= coins.P * 1;
+    }
+
+    return coins;
+
 }
